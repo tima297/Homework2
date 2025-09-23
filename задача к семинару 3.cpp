89a8018fc287197f15fd97bb1d4a91b9ddf11616
Timofey Leonchik <66067646+tima297@users.noreply.github.com>
2025-09-23 09:35:15 +0300

    Add files via upload

---
+++ задача к семинару 3.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include<vector>
+#include <cmath>
+using namespace std;
+int main()
+
+{
+    int a,c;
+    int summ = 0;
+    int max = 0;
+    int min=1000000;
+    int maxind = 0;
+    int minind = 0;
+    cout << "Write the scale of your massive:";
+    cin >> a;
+    int* array = new int[a];
+    int* newArray = new int[a];
+    int j = 0;
+    for (int i = 0; i < a; i++) {
+        cout << "write the " << i + 1 << " number:" << endl;
+        cin >> array[i];
+
+    }
+
+    cout << "Your massive:";
+    for (int i = 0; i < a; i++) {
+        cout << array[i];
+
+    }
+
+   
+    for (int i = 0; i < a; i++) {
+        if (array[i] > max) {
+            max = array[i];
+            maxind = i;
+        }
+        c = array[maxind];;
+        array[maxind] = array[a - 1];
+        array[a - 1] = c;
+
+    }
+
+    for (int i = 0; i < a; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            minind = i;
+        }
+        int  d = array[minind];;
+        array[minind] = array[0];
+        array[0] = d;
+    }
+
+
+
+    
+    cout << "Your new massive:";
+    for (int i = 0; i < a; i++) {
+        cout << array[i];
+
+    }
+    
+
+          
+
+   
+
+
+   // for (int i = 0; i < j; i++) {
+     //   if (newArray[i] > max)
+       //     max = newArray[i];
+
+    //}
+   // cout << "The biggest element from chetnue:" << max;
+
+
+
+
+    return 0;
+
+
+}
